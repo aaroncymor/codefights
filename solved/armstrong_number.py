ba6583f3f5f4e149cd0214054a8a447c47c37077
Aaron Cymor <aaroncymor@gmail.com>
2016-02-25 17:15:17 +0800

    Adding the armstrong challenge to solved folder. This was easy but I tried to implement list comprehension and it did not disappoint! I was able to reduce 91 lines of code to 68! It was fun!

---
+++ solved/armstrong_number.py
@@ -0,0 +1,25 @@
+"""
+An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
+Check if the given number is an Armstrong number.
+
+Example:
+
+    Armstrong_number(407) = true
+    43 + 03 + 73 = 64 + 343 = 407.
+    Armstrong_number(23) = false
+    22 + 32 = 13 ≠ 23.
+
+    [input] integer N
+        1 ≤ N ≤ 109
+
+    [output] boolean
+        true is the given number is an Armstrong number, false otherwise.
+
+"""
+
+def Armstrong_number(N):
+    n = str(N)
+    return sum([int(c) ** len(n) for c in n]) == N
+
+print Armstrong_number(407)
+print Armstrong_number(23)
