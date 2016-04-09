69abb9a0f4df9f83c3c5e4e4a5e857f9926950f3
Aaron Cymor <aaroncymor@gmail.com>
2016-04-09 23:00:50 +0800

    Adding count4s.py solved this problem in an inefficient way. Will try to understand the 'Dynamic programming way' to solve this.

---
+++ needs to be solved/count4s.py
@@ -0,0 +1,27 @@
+"""
+Find the total number of times digit 4 occurs in the range [0,n).
+
+Example:
+
+    For n = 14, the output should be
+    Count4s(n) = 1.
+    The only time 4 appears is in number 4.
+    For n = 15, the output should be
+    Count4s(n) = 2.
+    4 appears in 4 and 14.
+    For n = 44, the output should be
+    Count4s(n) = 8.
+    For n = 45, the output should be
+    Count4s(n) = 10.
+
+"""
+def Count4s(n):
+	counter = 0
+	for i in range(1,n):
+		while i != 0:
+			if i % 10 == 4:
+				counter += 1
+			i /= 10
+	return counter
+
+print Count4s(1000000000)
